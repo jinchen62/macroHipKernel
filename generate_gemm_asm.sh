6aae8a5cfde02d5fdc472a9c3312ceba191dd7fa
jinchen62 <jinchenye62@gmail.com>
2025-08-28 02:42:06 -0500

    Add preshuffle gemm asm kernel

--- generate_gemm_asm.sh
+++ generate_gemm_asm.sh
@@ -1,4 +1,4 @@
-rm $1.co
 ~/iree-build/llvm-project/bin/clang -target amdgcn-amd-amdhsa -mcpu=gfx950 -c $1 -o $1.o
 ~/iree-build/llvm-project/bin/lld -flavor gnu -shared $1.o -o $1.co
-rm $1.o
+xxd -p -c 1000000 $1.co > $1.hex
+rm $1.o $1.co
